d040445c947188d3313de195dd23d06a38955f32
sudo <digitalyunax@gmail.com>
2024-03-01 20:19:00 +0530

    Add the the styling

---
+++ src/ColorBox.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ColorBox = () => {
+  return (
+    <div className='box-wrapper'>
+     <div className="box"></div>
+    </div>
+  )
+}
+
+export default ColorBox
---
+++ src/ColorPicker.js
@@ -0,0 +1,16 @@
+const ColorPicker = () => {
+const handleclick=()=>{
+
+ }
+  return (
+    <form action="">
+      <input
+      autoFocus 
+      type="text" 
+      placeholder="Enter a color"
+      />
+    </form>
+  );
+};
+
+export default ColorPicker;
